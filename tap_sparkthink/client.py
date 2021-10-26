e2e0be9a8002928bb633d00d2be5e08713409582
alvin-mwangi <alvin.mwangi@slalom.com>
2021-10-25 17:04:21 -0700

    add variable for default response batch size

--- tap_sparkthink/client.py
+++ tap_sparkthink/client.py
@@ -36,6 +36,16 @@ class sparkthinkStream(GraphQLStream):
     def project_id(self) -> str:
         """Return the project_id."""
         return self.config.get("project_id")
+    
+    @property
+    def response_batch_size(self) -> str:
+        """Return the response_batch_size."""
+        return self.config.get("response_batch_size")
+    
+    @property
+    def response_default_batch_size(self) -> str:
+        """Return the project_id."""
+        return 10
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result rows."""
@@ -54,7 +64,11 @@ class ProjectBasedStream(sparkthinkStream):
         
         if "project" in self.records_jsonpath:
             return [
-                {"project_id": id} for id in [ x.strip() for x in self.config.get("project_ids").strip('[]').split(',') ]
+                {
+                    "project_id": id,
+                    "response_batch_size": self.response_batch_size or self.response_default_batch_size
+                } 
+                for id in [ x.strip() for x in self.config.get("project_ids").strip('[]').split(',') ]
             ]
 
         raise ValueError(
